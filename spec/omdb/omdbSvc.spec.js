362189a404b59410060f756f53dc244e4dce06fd
Mark Savage <mark@savagebits.com>
2016-02-03 09:38:23 -0600

    saving

--- spec/omdb/omdbSvc.spec.js
+++ spec/omdb/omdbSvc.spec.js
@@ -2,18 +2,25 @@ describe('omdb service', function() {
     var movieData = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"345","imdbID":"tt0251413","Type":"game","Response":"True"}
     var movieDataById = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"345","imdbID":"tt0251413","Type":"game","Response":"True"}
     var omdbApi = {};
+    var $httpBackend;
 
     //this is effectively angular.mock.module('omdb')
     // and angular.mock.inject(...) below
     //ngMock makes module() and inject() available on the window object
-    // so angular.mock is optional
+    // so angular.mock is optional except when calling dump
+    // because Karma also has a dump function which is shorthand for console.log
     beforeEach(
+        //this loads the module
         module('omdb')
     );
 
     beforeEach(
-        inject(function(_OmdbSvc_) {
+        //this grabs an instace of a component from the module we've loaded
+        //here in the callback function, we're assigning the result to a
+        // var in scope
+        inject(function(_OmdbSvc_, _$httpBackend_) {
             omdbApi = _OmdbSvc_;
+            $httpBackend = _$httpBackend_;
         })
     );
 
@@ -42,8 +49,14 @@ describe('omdb service', function() {
         //             }
         //         })
         //     });
+        var response;
 
-        expect(omdbApi.search('star wars episode iv')).toEqual(movieData);
+        omdbApi.search('star wars episode iv')
+            .then(function(data) {
+                response = data;
+            });
+
+        expect(response).toEqual(movieData);
     });
 
     it('should return movie data by id', function() {
--- src/omdb/omdbSvc.js
+++ src/omdb/omdbSvc.js
@@ -1,6 +1,17 @@
-function OmdbSvc() {
+var baseUrl = 'http://www.omdbapi.com/?v=1&';
+
+function OmdbSvc($http, $q) {
     OmdbSvc.search = function(query) {
-        return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"345","imdbID":"tt0251413","Type":"game","Response":"True"};
+        var deferred = $q.defer();
+
+        $http.get(baseUrl + 's=' + encodeURIComponent(query))
+            .success(function(data) {
+                deferred.resolve(data);
+            });
+
+        return deferred.promise;
+
+//        return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"345","imdbID":"tt0251413","Type":"game","Response":"True"};
     }
 
     OmdbSvc.find = function(id) {
